194840ca16f848b1aa2cd34cf43486ebe03456d9
Steeve15 <steevenmarooday2@gmail.com>
2024-06-20 19:53:35 +0400

    adding check feature

---
+++ check_ans.js
@@ -0,0 +1,10 @@
+function Good(){
+    alert("Very Good!!")
+}
+
+function Bad(){
+    alert("very bad")
+}
+
+document.querySelector(".good").addEventListener("click", Good);
+document.querySelector(".bad").addEventListener("click", Bad);
